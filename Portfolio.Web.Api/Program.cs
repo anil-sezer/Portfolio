081112eaf546dd690a7bf8ccaf748e27441571c9
Anıl Sezer <msnanil@hotmail.com>
2023-12-11 21:11:05 +0300

    refactor: Cleanup at Program.cs

--- Portfolio.Web.Api/Program.cs
+++ Portfolio.Web.Api/Program.cs
@@ -17,12 +17,8 @@ builder.DbInitWithPostgres();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -30,23 +26,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapPost("/data", async (HttpRequest request) =>
-{
-    using var reader = new StreamReader(request.Body);
-    var body = await reader.ReadToEndAsync();
-    var data = JsonSerializer.Deserialize<SensorData>(body);
-
-    // You would add your database logic here
-    // For now, we just log to the console
-    Console.WriteLine($"Temperature: {data?.Temperature}, Humidity: {data?.Humidity}");
-
-    return Results.Ok(new { message = "Data received successfully!" });
-});
-
 app.Run();
-
-public class SensorData
-{
-    public string Temperature { get; set; }
-    public string Humidity { get; set; }
-}

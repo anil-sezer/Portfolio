6a282843f73b6d96a31c357a0dc5f3455bb8f1fe
Anıl Sezer <msnanil@hotmail.com>
2024-01-12 20:45:21 +0300

    fix: temp: Updated service StoreDevicesReportAsync logic to accept json inside json, this is a temp and dirty fix

--- Portfolio.Web.Api/Services/Dto/ReportInput.cs
+++ Portfolio.Web.Api/Services/Dto/ReportInput.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using Portfolio.Domain.Constants;
 
 namespace Portfolio.Web.Api.Services.Dto;
@@ -6,5 +7,5 @@ namespace Portfolio.Web.Api.Services.Dto;
 public class ReportInput: IotBaseInput
 {
     [Required(ErrorMessage = Etcetera.DataAnnotation_RequiredErrorMsg)]
-    public string Report { get; set; }
+    public JsonElement Report { get; set; }
 }
--- Portfolio.Web.Api/Services/IotReportService.cs
+++ Portfolio.Web.Api/Services/IotReportService.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Domain.Entities.WebAppEntities;
+﻿using System.Text.Json.Nodes;
+using Portfolio.Domain.Entities.WebAppEntities;
 using Portfolio.Web.Api.Services.Dto;
 
 namespace Portfolio.Web.Api.Services;
@@ -14,12 +15,12 @@ public class IotReportService
 
     public async Task StoreDevicesReportAsync(ReportInput input)
     {
+        var jsonString = JsonObject.Create(input.Report).ToJsonString();
         var reportEntry = new IotReport
         {
             DeviceName = input.DeviceName,
-            Report = input.Report
+            Report = jsonString
         };
-
         await _dbContext.IotReport.AddAsync(reportEntry);
         await _dbContext.SaveChangesAsync();
     }

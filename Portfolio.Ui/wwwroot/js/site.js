d6f0b0b0d70df68ff9eea26a9eba937ca6cef597
Anıl Sezer <msnanil@hotmail.com>
2025-01-20 15:12:18 +0300

    docs: Removed credits, because website had malicious code in it: https://github.com/advisories/GHSA-p3hc-fv2j-rp68

--- Portfolio.Ui/wwwroot/js/site.js
+++ Portfolio.Ui/wwwroot/js/site.js
@@ -1,15 +1,5 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-/**
- * Template Name: iPortfolio
- * Updated: May 30 2023 with Bootstrap v5.3.0
- * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function() {
     "use strict";
 

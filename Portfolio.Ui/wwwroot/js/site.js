51018fe9e80e3a464e47885e883dd6159eab5124
Anıl Sezer <msnanil@hotmail.com>
2025-01-20 12:03:19 +0300

    feat: UI: glightbox library is removed, it was unused

--- Portfolio.Ui/wwwroot/js/site.js
+++ Portfolio.Ui/wwwroot/js/site.js
@@ -195,13 +195,6 @@
     });
 
     /**
-     * Initiate portfolio lightbox
-     */
-    const portfolioLightbox = GLightbox({
-        selector: '.portfolio-lightbox'
-    });
-
-    /**
      * Portfolio details slider
      */
     new Swiper('.portfolio-details-slider', {

8b747a5194b3c52640959939330be40d1b4f1db4
Anıl Sezer <msnanil@hotmail.com>
2025-01-22 11:04:33 +0300

    chore: ui: Tidied up a bit

--- Portfolio.Ui/wwwroot/js/site.js
+++ Portfolio.Ui/wwwroot/js/site.js
@@ -3,6 +3,10 @@
 (function() {
     "use strict";
 
+    /*--------------------------------------------------------------
+    # TODO: OLD CODE, CHECK OUT
+    --------------------------------------------------------------*/
+
     /**
      * Easy selector helper function
      */
@@ -34,37 +38,6 @@
      */
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
-    }
-
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-        let position = window.scrollY + 200
-        navbarlinks.forEach(navbarlink => {
-            if (!navbarlink.hash) return
-            let section = select(navbarlink.hash)
-            if (!section) return
-            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-                navbarlink.classList.add('active')
-            } else {
-                navbarlink.classList.remove('active')
-            }
-        })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-            top: elementPos,
-            behavior: 'smooth'
-        })
     }
 
     /**

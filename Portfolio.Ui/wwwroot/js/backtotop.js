12cdbce681a5abf22dba9d4c730d0d9d10babd1e
Anıl Sezer <msnanil@hotmail.com>
2025-01-22 14:25:03 +0300

    fix: ui: small fixes & tidied code a bit

---
+++ Portfolio.Ui/wwwroot/js/backtotop.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    if (backToTopButton) {
+        const toggleBackToTopButton = () => {
+            if (window.scrollY > 100) {
+                backToTopButton.classList.add('active');
+            } else {
+                backToTopButton.classList.remove('active');
+            }
+        };
+
+        window.addEventListener('load', toggleBackToTopButton);
+        window.addEventListener('scroll', toggleBackToTopButton);
+    }
+})();
---
+++ Portfolio.Ui/wwwroot/js/mobilenavtoggle.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('body').classList.toggle('mobile-nav-active')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+})();

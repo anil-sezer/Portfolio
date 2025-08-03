deb83e4fb34eb6638cbec4042185c653cf237bf5
msnan <msnanil@hotmail.com>
2025-08-03 19:49:20 +0300

    feat: ClientVisit logging is a seperated component now and it will use queue instead of delaying response time

---
+++ Portfolio.Grpc/BackgroundServices/LogVisitorInfoQueuedOperation.cs
@@ -0,0 +1,36 @@
+﻿using Portfolio.Domain.Interfaces.BackgroundServices;
+using Portfolio.Domain.Interfaces.Repositories;
+
+namespace Portfolio.Grpc.BackgroundServices;
+
+public class LogVisitorInfoQueuedOperation : IDatabaseOperationQueueWorker
+{
+    public required StoreVisitorInfoRequest Request { get; init; }
+
+    public async Task ExecuteAsync(IServiceProvider serviceProvider)
+    {
+        var repo = serviceProvider.GetRequiredService<IRequestLogRepository>();
+        
+        await repo.CreateAsync(new RequestLog
+        {
+            AcceptLanguage = Request.Language,
+            UserAgent = Request.UserAgent,
+            Platform = Request.Platform,
+            Referrer = Request.Referrer,
+            DoNotTrack = Request.DoNotTrack,
+            Connection = Request.Connection,
+            Resolution = Request.Resolution,
+            DeviceMemory = Request.DeviceMemory,
+            OnLine = Request.OnLine,
+            HardwareConcurrency = Request.HardwareConcurrency,
+            Webdriver = Request.Webdriver,
+            CookieEnabled = Request.CookieEnabled,
+            MaxTouchPoints = Request.MaxTouchPoints,
+
+            ClientIp = Request.IpAddress,
+            City = string.Empty,
+            Country = string.Empty,
+            Extras = Request.Extras
+        });
+    }
+}
--- Portfolio.Grpc/Services/VisitorInsightsServices/VisitorInsightsService.StoreVisitorInfo.cs
+++ Portfolio.Grpc/Services/VisitorInsightsServices/VisitorInsightsService.StoreVisitorInfo.cs
@@ -1,37 +1,39 @@
 using System.Text.Json;
+using Portfolio.Grpc.BackgroundServices;
 
 namespace Portfolio.Grpc.Services.VisitorInsightsServices;
 
 public partial class VisitorInsightsService
 {
-    // todo: I wanna use MediatR here
     public override async Task<Empty> StoreVisitorInfo(StoreVisitorInfoRequest r, ServerCallContext context)
     {
         Log.Information("Request to log: {Log}",JsonSerializer.Serialize(r));
+        
+        await DatabaseOperationQueueWorker.EnqueueAsync(new LogVisitorInfoQueuedOperation{Request = r});
 
-        await requestLogRepository.CreateAsync(
-            new RequestLog
-            {
-                AcceptLanguage = r.Language,
-                UserAgent = r.UserAgent,
-                Platform = r.Platform,
-                Referrer = r.Referrer,
-                DoNotTrack = r.DoNotTrack,
-                Connection = r.Connection,
-                Resolution = r.Resolution,
-                DeviceMemory = r.DeviceMemory,
-                OnLine = r.OnLine,
-                HardwareConcurrency = r.HardwareConcurrency,
-                Webdriver = r.Webdriver,
-                CookieEnabled = r.CookieEnabled,
-                MaxTouchPoints = r.MaxTouchPoints,
-
-                ClientIp = r.IpAddress,
-                City = string.Empty,
-                Country = string.Empty,
-                Extras = r.Extras
-            }
-        );
+        // await requestLogRepository.CreateAsync(
+        //     new RequestLog
+        //     {
+        //         AcceptLanguage = r.Language,
+        //         UserAgent = r.UserAgent,
+        //         Platform = r.Platform,
+        //         Referrer = r.Referrer,
+        //         DoNotTrack = r.DoNotTrack,
+        //         Connection = r.Connection,
+        //         Resolution = r.Resolution,
+        //         DeviceMemory = r.DeviceMemory,
+        //         OnLine = r.OnLine,
+        //         HardwareConcurrency = r.HardwareConcurrency,
+        //         Webdriver = r.Webdriver,
+        //         CookieEnabled = r.CookieEnabled,
+        //         MaxTouchPoints = r.MaxTouchPoints,
+        //
+        //         ClientIp = r.IpAddress,
+        //         City = string.Empty,
+        //         Country = string.Empty,
+        //         Extras = r.Extras
+        //     }
+        // );
         
         return new Empty();
     }
